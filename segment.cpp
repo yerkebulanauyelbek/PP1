0020112874e25d18140d6031b63f777c62f7022b
Yerkebulan Auyelbek <78300056+yerkebulanauyelbek@users.noreply.github.com>
2022-04-01 02:22:31 +0600

    Delete segment.cpp

--- segment.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main(){
-    int x1,y1,x2,y2;double seg; cin>>x1>>y1>>x2>>y2;
-    seg=sqrt((pow((x1-x2),2))-(pow((y1-y2),2)));
-    cout<<seg;
-}
